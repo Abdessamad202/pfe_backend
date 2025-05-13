a97b361d9bf842c92971a34ffc17ae8abeb982f1
abdessamad kech-chaf <kechaf.abdo.ka.2018@gmail.com>
2025-05-13 12:12:26 +0100

    Refactor User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,47 +2,34 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+    protected $fillable = ['name', 'email', 'password', 'role'];
+    public function marques()
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return $this->hasMany(Marque::class);
     }
+    public function produits()
+    {
+        return $this->hasMany(Produit::class);
+    }
+    public function typesDeProduits()
+    {
+        return $this->hasMany(TypeProduit::class);
+    }
+
+    public function achats()
+    {
+        return $this->hasMany(Achat::class);
+    }
+    public function ventes()
+    {
+        return $this->hasMany(Vente::class);
+    }
+    // public function stocks()
+    // {
+    //     return $this->hasMany(Stock::class);
+    // }
 }
