f946f5d73f4ccf6535e03cb8a039eec695afe26a
abdessamad kech-chaf <kechaf.abdo.ka.2018@gmail.com>
2025-05-13 12:12:24 +0100

    Refactor Produit model

--- app/Models/Produit.php
+++ app/Models/Produit.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Produit extends Model
 {
-    //
+    protected $fillable = ['nom', 'prix'];
+
+    public function typeProduit()
+    {
+        return $this->belongsTo(TypeProduit::class);
+    }
+
+    public function marque()
+    {
+        return $this->belongsTo(Marque::class);
+    }
+
+    public function achats()
+    {
+        return $this->belongsToMany(Achat::class, 'achat_produits')->withPivot('quantite', 'prix_unitaire');
+    }
+    public function ventes()
+    {
+        return $this->belongsToMany(Vente::class, 'vente_produits')->withPivot('quantite', 'prix_unitaire');
+    }
+    public function tvs()
+    {
+        return $this->hasMany(Tv::class);
+    }
 }
