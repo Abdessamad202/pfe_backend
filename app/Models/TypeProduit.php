945b6fc3658166767515e1385254658736b9d544
abdessamad kech-chaf <kechaf.abdo.ka.2018@gmail.com>
2025-05-13 12:12:25 +0100

    Refactor TypeProduit model

--- app/Models/TypeProduit.php
+++ app/Models/TypeProduit.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class TypeProduit extends Model
 {
-    //
+    protected $fillable = ['nom'];
+
+    public function produits()
+    {
+        return $this->hasMany(Produit::class);
+    }
 }
