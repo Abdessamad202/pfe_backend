37f9a38009df4180caa8e48b612d93a476e4771e
abdessamad kech-chaf <kechaf.abdo.ka.2018@gmail.com>
2025-05-13 12:12:22 +0100

    Refactor Achat model

--- app/Models/Achat.php
+++ app/Models/Achat.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Achat extends Model
 {
-    //
+    protected $fillable = ['user_id', 'fournisseur_id', 'date'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function fournisseur()
+    {
+        return $this->belongsTo(Fournisseur::class);
+    }
+
+    public function produits()
+    {
+        return $this->belongsToMany(Produit::class, 'achat_produits')->withPivot('quantite', 'prix_unitaire');
+    }
 }
