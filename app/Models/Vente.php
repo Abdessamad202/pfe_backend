2d7134d8564314f35d42f09d22f8f7cb4470b087
abdessamad kech-chaf <kechaf.abdo.ka.2018@gmail.com>
2025-05-13 12:12:26 +0100

    Refactor Vente model

--- app/Models/Vente.php
+++ app/Models/Vente.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Vente extends Model
 {
-    //
+    protected $fillable = ['user_id', 'date'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function produits()
+    {
+        return $this->belongsToMany(Produit::class, 'vente_produits')->withPivot('quantite', 'prix_unitaire');
+    }
 }
