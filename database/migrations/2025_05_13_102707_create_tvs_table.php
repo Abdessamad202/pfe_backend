c00746038b2d5d376853a8a40607405aa1cd586d
abdessamad kech-chaf <kechaf.abdo.ka.2018@gmail.com>
2025-05-13 11:41:53 +0100

    Update tvs table migration

--- database/migrations/2025_05_13_102707_create_tvs_table.php
+++ database/migrations/2025_05_13_102707_create_tvs_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('tvs', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('produit_id')->constrained('produits');
+            $table->string('barcode');
+            $table->string('serial_number');
             $table->timestamps();
         });
     }
