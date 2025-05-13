757981474bc1ef57b0f8b69d4e4cd2d6cd2ffa4e
abdessamad kech-chaf <kechaf.abdo.ka.2018@gmail.com>
2025-05-13 12:12:27 +0100

    Refactor VenteProduit model

--- app/Models/VenteProduit.php
+++ app/Models/VenteProduit.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class VenteProduit extends Model
+class VenteProduit extends Pivot
 {
-    //
+    protected $table = 'vente_produits';
+
+    protected $fillable = ['vente_id', 'produit_id', 'quantite', 'prix_unitaire'];
 }
