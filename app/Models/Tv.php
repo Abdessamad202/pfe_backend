18ff1bdc3f8c863919d3e15b3f01bb013d22a523
abdessamad kech-chaf <kechaf.abdo.ka.2018@gmail.com>
2025-05-13 12:12:25 +0100

    Refactor Tv model

--- app/Models/Tv.php
+++ app/Models/Tv.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tv extends Model
 {
-    //
+    // protected $fillable =  ['taux'];
+
+    public function produit()
+    {
+        return $this->belongsTo(Produit::class);
+    }
 }
