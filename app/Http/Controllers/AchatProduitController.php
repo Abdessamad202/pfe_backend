217ab732d25502e947e3a13f1caaaf5244b5486e
abdessamad kech-chaf <kechaf.abdo.ka.2018@gmail.com>
2025-05-13 11:29:03 +0100

    initialise the project

---
+++ app/Http/Controllers/AchatProduitController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AchatProduit;
+use Illuminate\Http\Request;
+
+class AchatProduitController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(AchatProduit $achatProduit)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(AchatProduit $achatProduit)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, AchatProduit $achatProduit)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(AchatProduit $achatProduit)
+    {
+        //
+    }
+}
