80e2bd7a99a64b9ea6b4ba5eda9658353c372f84
abdessamad kech-chaf <kechaf.abdo.ka.2018@gmail.com>
2025-05-13 12:12:23 +0100

    Refactor Marque model

--- app/Models/Marque.php
+++ app/Models/Marque.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Marque extends Model
 {
-    //
+    protected $fillable = ['nom'];
+    public function typesDeProduits()
+    {
+        return $this->hasMany(TypeProduit::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
