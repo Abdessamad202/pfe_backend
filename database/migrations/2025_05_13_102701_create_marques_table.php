57cd3e6196666a47b53668e895fa22b53a55235c
abdessamad kech-chaf <kechaf.abdo.ka.2018@gmail.com>
2025-05-13 11:41:50 +0100

    Update marques table migration

--- database/migrations/2025_05_13_102701_create_marques_table.php
+++ database/migrations/2025_05_13_102701_create_marques_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('marques', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('nom');
             $table->timestamps();
         });
     }
